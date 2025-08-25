2b132dc76c698634eb95b3ad9a8b6a4de20c1949
ryanstewart047 <ryanstewart047@gmail.com>
2025-08-25 15:45:30 -0700

    Deploy to GitHub Pages: Mon Aug 25 15:45:30 MST 2025

--- freelance_platform/start_backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-cd "/Users/new/Freelance Web Application/freelance_platform" 
-python3 -m venv venv || true
-source venv/bin/activate
-pip3 install -r requirements.txt
-cd backend
-python3 -m flask run
